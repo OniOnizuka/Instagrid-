bb9f79408ca57ba7ad02e7db2fb8599962a7e314
Alexandre <65859717+OniOnizuka@users.noreply.github.com>
2020-11-02 19:53:18 +0100

    Add files via upload

---
+++ P4-Alexander Nys/InstagridP4/InstagridP4/Extensions/UIViewExtension.swift
@@ -0,0 +1,28 @@
+//
+//  LandScape.swift
+//  Instagrid
+//
+//  Created by ONIZUKA  on 30/10/2020.
+//  Copyright © 2020 ONIZUKA . All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+//MARK: -Transfrom mainBlueView to UIImage object
+
+
+extension UIView {
+    
+    var image : UIImage {
+        
+        let renderer = UIGraphicsImageRenderer(size: self.bounds.size)
+        
+        let image = renderer.image { i  in
+            
+            self.drawHierarchy(in: self.bounds, afterScreenUpdates: true)
+        }
+        return image
+    }
+}
