9bd64c1f9d34952369444554e5f7539a549016f3
Alexandre <65859717+OniOnizuka@users.noreply.github.com>
2020-11-05 19:31:27 +0100

    Add files via upload
    Instagrid updated

---
+++ Instagrid--main/P4-Alexander Nys/InstagridP4/InstagridP4/Controller/ViewController.swift
@@ -0,0 +1,93 @@
+//
+//  ViewController.swift
+//  InstagridP4
+//
+//  Created by ONIZUKA  on 02/11/2020.
+//  Copyright © 2020 ONIZUKA . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController,UIImagePickerControllerDelegate {
+    
+    var swipeGestureIdentification: UISwipeGestureRecognizer?
+    
+    // The variable used to pick an image from the user's photo library.
+    private var   buttonTouched = UIButton()
+    private let imageChoice = UIImagePickerController()
+    
+    
+    //MARK: -Outlet Connection
+    
+    
+    @IBOutlet weak var topLeftButton: UIButton!
+    @IBOutlet weak var topRightButton: UIButton!
+    @IBOutlet weak var bottomLeftButton: UIButton!
+    @IBOutlet weak var bottomRightButton: UIButton!
+    
+    @IBOutlet weak var Layout1Button: UIButton!
+    @IBOutlet weak var Layout2Button: UIButton!
+    @IBOutlet weak var Layout3Button: UIButton!
+    
+    @IBOutlet weak var swipeUpOrLeft: UILabel!
+    
+    @IBOutlet weak var mainView: UIView!
+    
+    
+    //MARK:- viewDidLoad
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        Layout2Button.isSelected = true
+        bottomLeftButton.isHidden = true
+        imageChoice.delegate = self
+        swipeGestureIdentification = UISwipeGestureRecognizer(target: self, action: #selector(swipeOutMainBlueView(sender:)))
+        guard let swipeGesture = swipeGestureIdentification else {fatalError()}
+        mainView.addGestureRecognizer(swipeGesture)
+        NotificationCenter.default.addObserver(self, selector: #selector(swipeOrder), name: UIDevice.orientationDidChangeNotification, object: nil)
+    }
+    
+    
+    //MARK: - Image insertion
+    
+    
+    @IBAction func imageButton(_ sender: UIButton) {
+        buttonTouched = sender
+        takePhoto()
+        
+    }
+    
+    func takePhoto() {
+        if (UIImagePickerController.isSourceTypeAvailable(.photoLibrary)) {
+            imageChoice.delegate = self
+            imageChoice.sourceType = .photoLibrary
+            imageChoice.allowsEditing = false
+            present(imageChoice, animated: true, completion: nil)
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        picker.dismiss(animated: true, completion: nil)
+        if let pickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage { imageInsertion(pickedImage) }
+        
+    }
+    
+    func imageInsertion(_ image: UIImage) {
+        buttonTouched.contentMode = .scaleAspectFit
+        buttonTouched.setImage(image, for: UIControl.State.normal)
+    }
+    
+    
+    //MARK: - Swipe Gesture for Sharing
+    
+    
+    @objc func swipeOrder() {
+        if UIDevice.current.orientation == .portrait {
+            swipeGestureIdentification?.direction = .up
+        } else if UIDevice.current.orientation == .landscapeLeft || UIDevice.current.orientation == .landscapeRight {
+            swipeGestureIdentification?.direction = .left
+        }
+    }
+}
+
---
+++ Instagrid--main/P4-Alexander Nys/InstagridP4/InstagridP4/Extensions/ViewControllerExtension.swift
@@ -0,0 +1,112 @@
+//
+//  ViewControllerExtension.swift
+//  Instagrid
+//
+//  Created by ONIZUKA  on 30/10/2020.
+//  Copyright © 2020 ONIZUKA . All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension ViewController : UINavigationControllerDelegate {
+    
+    
+    //MARK: - Label Portrait/Landscape modification
+    
+    
+    @IBAction func Layout1ButtonTouched(_ sender: Any) {
+        if (Layout1Button.currentImage == nil) {
+            removeButtonsImages()
+            bottomRightButton.isEnabled = true;
+            bottomRightButton.isHidden = false;
+            Layout1Button.setImage(UIImage(named: "Selected"), for: UIControl.State.normal)
+            topRightButton.isEnabled = true;
+            topRightButton.isHidden = true;
+            bottomLeftButton.isHidden = false;
+        }
+    }
+    
+    @IBAction func Layout2ButtonTouched(_ sender: Any) {
+        if (Layout2Button.currentImage == nil) {
+            removeButtonsImages()
+            topRightButton.isEnabled = true;
+            topRightButton.isHidden = false;
+            Layout2Button.setImage(UIImage(named: "Selected"), for: UIControl.State.normal)
+            bottomRightButton.isEnabled = true;
+            bottomRightButton.isHidden = false;
+            bottomLeftButton.isHidden = true;
+            
+        }
+    }
+    
+    @IBAction func Layout3ButtonTouched(_ sender: Any) {
+        if (Layout3Button.currentImage == nil) {
+            removeButtonsImages()
+            topRightButton.isEnabled = true;
+            bottomRightButton.isEnabled = true;
+            topRightButton.isHidden = false;
+            bottomRightButton.isHidden = false;
+            bottomLeftButton.isHidden = false;
+            Layout3Button.setImage(UIImage(named: "Selected"), for: UIControl.State.normal)
+        }
+    }
+    
+    func removeButtonsImages() {
+        Layout1Button.setImage(nil, for: UIControl.State.normal)
+        Layout2Button.setImage(nil, for: UIControl.State.normal)
+        Layout3Button.setImage(nil, for: UIControl.State.normal)
+    }
+    
+    
+    //MARK: -Swipe Gesture for Sharing
+    
+    
+    @objc func swipeOutMainBlueView(sender: UISwipeGestureRecognizer) {
+        if sender.state == .ended {
+            switch sender.direction {
+            case .up :
+                swipeUp(gesture: sender)
+            case .left :
+                swipeLeft(gesture: sender)
+            default :
+                break
+            }
+        }
+    }
+    
+    fileprivate func swipeUp(gesture: UISwipeGestureRecognizer) {
+        
+        UIView.animate(withDuration: 0.5, animations: {
+            self.mainView.transform = CGAffineTransform(translationX: 0, y: -UIScreen.main.bounds.height)
+        }, completion: {
+            (true) in
+            self.shareView()
+        })
+    }
+    
+    fileprivate func swipeLeft(gesture: UISwipeGestureRecognizer) {
+        
+        UIView.animate(withDuration: 0.5, animations: {
+            self.mainView.transform = CGAffineTransform(translationX: -UIScreen.main.bounds.width, y: 0)
+        }, completion: {
+            (true) in
+            self.shareView()
+        })
+    }
+    
+    
+    //MARK: -Share Sheet Animation + reset mainBlueView after sharing
+    
+    
+    fileprivate func shareView() {
+        
+        let items = [mainView.image]
+        let activityController = UIActivityViewController(activityItems: items, applicationActivities: nil)
+        present(activityController, animated: true)
+        activityController.completionWithItemsHandler = { activity, sucess, items, error in
+            UIView.animate(withDuration: 1, animations: {
+                self.mainView.transform = .identity }
+            )}
+    }
+}
